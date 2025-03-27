888aa10f049fb648697a96becae13f396646bfcd
silve <silve@SILVERNOTE>
2025-03-26 21:31:54 -0300

    Adicionar arquivos de projeto.

---
+++ Desafios/Program.cs
@@ -0,0 +1,11 @@
+﻿// ------------------------------  Aula 1 ------------------------------  
+string curso = "Curso de C#";
+Console.WriteLine(curso);
+
+string nome = "Igor";
+string sobrenome = "Silveira";
+
+Console.WriteLine(nome + " " + sobrenome);
+
+//----------------------------------------------------------------------
+
